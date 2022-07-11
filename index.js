d655fccaa322a4d9d72efed7bf74d245e7351cdd
remishn <nsarem65@gmail.com>
2022-07-10 19:50:26 -0500

    add html output

--- index.js
+++ index.js
@@ -6,70 +6,100 @@ const intern = require('./tests/intern.test')
 
 const employees = []
 
-function addTeamMember() {
-    inquirer.prompt([{
-        message: 'enter member name',
-        name :'name'
-    },
-    {
-        type: 'list',
-        message: 'select member role',
-        choices: ['engineer', 'manager', 'intern'],
-        name: 'role'
-    },
-    {
-        message: 'enter member id',
-        name: 'id'
-    },
-    {
-        message: 'enter member email address',
-        name: 'email'
-    },
+function generateProfile () {
+    startHtml()
+    addTeamMember()
+}
 
-])
-.then(function({name, role, id, email}) {
-    let roleInfo = ""
-    if (role === 'engineer') {
-        roleInfo = 'github username'
-    } else if (role === 'intern') {
-        roleInfo = 'school name'
-    } else {
-        roleInfo = 'office phone number'
-    }
-    inquirer.prompt([{
-        message: `Enter member ${roleInfo}`,
-        name: 'roleInfo'
-    },
-    {
-        type: 'list',
-        message: 'would you like to add more members?',
-        choices: ['yes', 'no'],
-        name: 'moreMembers'
-    }
-])
-.then(function({roleInfo, moreMembers}) {
-    let newMember;
-    if (role === "engineer") {
-        newMember = new engineer(name, id, email, roleInfo);
-    } else if (role === "Intern") {
-        newMember = new intern(name, id, email, roleInfo);
-    } else {
-        newMember = new manager(name, id, email, roleInfo);
-    }
-    employees.push(newMember);
-    addHtml(newMember)
-    .then(function() {
-        if (moreMembers === "yes") {
-            addMember();
-        } else {
-            console.log("done")
-            // finishHtml();
-        }
-        })
-    }
+async function addTeamMember() {
+    const {name, role, id, email} = await inquirer.prompt(
+        [
+            {
+                message: 'enter member name',
+                name :'name'
+            },
+            {
+                type: 'list',
+                message: 'select member role',
+                choices: ['engineer', 'manager', 'intern'],
+                name: 'role'
+            },
+            {
+                message: 'enter member id',
+                name: 'id'
+            },
+            {
+                message: 'enter member email address',
+                name: 'email'
+            },
+        ]
     )
+
+    let info = ""
+    if (role === 'engineer') {
+        info = 'github username'
+    } else if (role === 'intern') {
+        info = 'school name'
+    } else {
+        info = 'office phone number'
+    }
+
+    const {roleInfo, moreMembers} = await inquirer.prompt(
+        [
+            {
+                message: `Enter member ${info}`,
+                name: 'roleInfo'
+            },
+            {
+                type: 'list',
+                message: 'would you like to add more members?',
+                choices: ['yes', 'no'],
+                name: 'moreMembers'
+            }
+        ]
+    )
+
+    employees.push(
+        {
+            name: name,
+            role: role,
+            id : id,
+            email: email,
+            roleInfo: {
+                label: info,
+                value: roleInfo
+            }
+        }
+    )
+
+    console.log(employees)
+
+    if (moreMembers === 'yes') {
+        addTeamMember()
+    }
+    
+    function startHtml() {
+        const html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">            <title>Team Profile</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-dark bg-dark mb-5">
+                <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+            </nav>
+            <div class="container">
+                <div class="row">`;
+        fs.writeFile("./output/member.html", html, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        console.log("start");
+    }
 }
 
-)
-}
-addTeamMember()
+generateProfile()
